1863f8e0d41d0363357a9553b82f9357a05791e3
MaXqIvanov <maksivanov35@ya.ru>
2022-11-20 23:04:43 +0300

    fix path server

--- server/index.ts
+++ server/index.ts
@@ -6,13 +6,13 @@ const path = require('path');
 const app = express();
 const router = require('./routes')
 
-app.use(express.static(path.resolve('../build')));
+app.use(express.static(path.resolve('../client/build')));
 app.use(cors())
 app.use(express.json({limit: '100mb'}))
 app.use(express.urlencoded());
 app.use('/api', router)
 app.get('/*', (req: Request, res: Response) => {
-    res.sendFile(`../build/index.html`);
+    res.sendFile(`../client/build/index.html`);
 });
 
 app.listen(8000, () => {
